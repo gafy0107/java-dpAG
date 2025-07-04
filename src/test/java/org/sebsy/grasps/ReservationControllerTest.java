fd4ccf3412f9817d6638d2a7591d2977ea960a42
Aimé <gueiaime.y@gmail.com>
2025-07-04 14:24:08 +0200

    test

--- src/test/java/org/sebsy/grasps/ReservationControllerTest.java
+++ src/test/java/org/sebsy/grasps/ReservationControllerTest.java
@@ -1,74 +1,73 @@
 package org.sebsy.grasps;
- 
-import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+import org.junit.Test;
 import org.sebsy.grasps.beans.Reservation;
- 
-import static org.junit.jupiter.api.Assertions.*;
- 
-public class ReservationControllerTest {
- 
+
+public class ReservationControllerTest extends TestCase {
+
+    /**
+     * DELTA
+     */
+    private static final double DELTA = 0.0000001;
+
     @Test
-    public void testCreerReservation_ClientNonPremium_Cinema() {
+    public void testCreerReservationTheatrePremium() {
+
         Params params = new Params();
-        params.setIdentifiantClient("3");
-        params.setDateReservation("04/07/2025 20:00:00");
-        params.setTypeReservation("CI");
-        params.setNbPlaces(2);
- 
-        ReservationController controller = new ReservationController();
-        Reservation reservation = controller.creerReservation(params);
- 
-        assertNotNull(reservation);
-        assertFalse(reservation.getClient().isPremium());
- 
-        double expectedTotal = 2 * 10.9;
-        assertEquals(expectedTotal, reservation.getTotal(), 0.001);
-    }
- 
-    @Test
-    public void testCreerReservation_ClientPremium_Theatre() {
-        Params params = new Params();
-        params.setIdentifiantClient("1");
-        params.setDateReservation("04/07/2025 21:00:00");
-        params.setTypeReservation("TH");
+        params.setDateReservation("20/11/2020 19:55:00");
         params.setNbPlaces(3);
- 
+        params.setIdentifiantClient("1");
+        params.setTypeReservation("TH"); // Théâtre
+
         ReservationController controller = new ReservationController();
         Reservation reservation = controller.creerReservation(params);
- 
-        assertNotNull(reservation);
-        assertTrue(reservation.getClient().isPremium());
- 
-        double montant = 150.0;
-        double reduction = 15.0;
-        double expectedTotal = 3 * montant * (1 - reduction / 100.0);
- 
-        assertEquals(expectedTotal, reservation.getTotal(), 0.001);
+
+        assertEquals(382.5, reservation.getTotal(), DELTA);
     }
- 
+
     @Test
-    public void testCreerReservation_TypeReservationInexistant() {
+    public void testCreerReservationTheatreNonPremium() {
+
         Params params = new Params();
-        params.setIdentifiantClient("1");
-        params.setDateReservation("04/07/2025 21:00:00");
-        params.setTypeReservation("ZZ");
-        params.setNbPlaces(1);
- 
+        params.setDateReservation("20/11/2020 19:55:00");
+        params.setNbPlaces(3);
+        params.setIdentifiantClient("3");
+        params.setTypeReservation("TH"); // Théâtre
+
         ReservationController controller = new ReservationController();
- 
-        assertThrows(NullPointerException.class, () -> controller.creerReservation(params));
+        Reservation reservation = controller.creerReservation(params);
+
+        assertEquals(450.0, reservation.getTotal(), DELTA);
     }
- 
+
     @Test
-    public void testCreerReservation_ClientInexistant() {
+    public void testCreerReservationCinemaPremium() {
+
         Params params = new Params();
-        params.setIdentifiantClient("999");
-        params.setDateReservation("04/07/2025 21:00:00");
-        params.setTypeReservation("CI");
-        params.setNbPlaces(1);
- 
+        params.setDateReservation("21/11/2020 20:30:00");
+        params.setNbPlaces(4);
+        params.setIdentifiantClient("2");
+        params.setTypeReservation("CI"); // Théâtre
+
         ReservationController controller = new ReservationController();
- 
-        assertThrows(NullPointerException.class, () -> controller.creerReservation(params));
+        Reservation reservation = controller.creerReservation(params);
+
+        assertEquals(43.6, reservation.getTotal(), DELTA);
+    }
+
+    @Test
+    public void testCreerReservationCinemaNonPremium() {
+
+        Params params = new Params();
+        params.setDateReservation("21/11/2020 20:30:00");
+        params.setNbPlaces(4);
+        params.setIdentifiantClient("3");
+        params.setTypeReservation("CI"); // Théâtre
+
+        ReservationController controller = new ReservationController();
+        Reservation reservation = controller.creerReservation(params);
+
+        assertEquals(43.6, reservation.getTotal(), DELTA);
     }
 }
